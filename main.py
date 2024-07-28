3c78c0578c624261f160b29ae9dfa95a7dbbdcc7
Daksh021 <143600176+Daksh021@users.noreply.github.com>
2024-07-29 01:10:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pickle
+
+movies_list = pickle.load(open('movies.pkl') , 'rb')
+st.title("Movie Recommender System")
+option = st.selectbox(
+    'How would you like to be contacted ?',
+    ('Email' , 'Mobile Phone Number')
+)
